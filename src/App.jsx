ed808dedddf1c651491ef48f59af1380d0f432cd
Lukas Klipp <klipp.lukas@gmail.com>
2024-08-05 11:29:21 +0200

    first task

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
+import { ColorForm } from "./Components/ColorForm/ColorForm";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+  function handleSubmit(newColor) {
+    setColors((prevColors) => {
+      return [{ ...newColor, id: uid() }, ...prevColors];
+    });
+  }
   return (
     <>
+      <ColorForm handleSubmit={handleSubmit} />
       <h1>Theme Creator</h1>
 
-      {initialColors.map((color) => {
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
